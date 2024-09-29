d8d14ef7e8b976f20feb7b5e1d67f8e749aceab2
PingHsunTsai <47770211+PingHsunTsai@users.noreply.github.com>
2024-09-30 01:26:16 +0200

    dfs

--- backend/src/controllers/graph_control.js
+++ backend/src/controllers/graph_control.js
@@ -6,7 +6,7 @@ const INF = 2147483647;
 
 class Graph {
   constructor() {
-    this.friendsAdjList = new Map();
+    this.adjList = new Map();
     this.startUserIds = 2;
     this.users = [];
     this.userMap = new Map();
@@ -23,83 +23,85 @@ class Graph {
     this.users.forEach(user => {
         this.userMap.set(user.id, user);
         this.visitedFriends.set(user.id, false);
+        this.addVertex(user.id);
     });
   }
 
   updateGraph = async () => {
     await this.fetchData();
-    await this.friendGraph();
+    this.friendGraph();
 
-    const dijkstrMap = Object.fromEntries(this.dijkstra(this.friendsAdjList, this.startUserIds));
-    const objAdjList = Object.fromEntries(this.friendsAdjList);
+    const dfsMap = this.dfs(this.startUserIds);
+    const dijkstrMap = Object.fromEntries(this.dijkstra(this.startUserIds));
+    const objAdjList = Object.fromEntries(this.adjList);
 
     const outputGraph = {
         adjList: objAdjList,
         dijkstrMap: dijkstrMap,
+        dfsMap: dfsMap
     };
 
     return outputGraph;
   }
 
-  friendGraph = async () => {
-
+  friendGraph = () => {
 
     this.friends.forEach((friend) => {
         const user1 = this.userMap.get(friend.userId);
         const user2 = this.userMap.get(friend.friendId);
 
         if (user1 && user2) {
-            const weight = this.calculateWeight(user1, user2, true);
-            this.addEdge(this.friendsAdjList, friend.userId, friend.friendId, weight);
+            const weight = this.calculateWeight(user1, user2);
+            this.addEdge(friend.userId, friend.friendId, weight);
         }
     });
-
     console.log('friendGraph updated successfully.');
   };
 
-  calculateWeight = (user1, user2, friend) => {
-    let weight = 0;
+  strangersGraph = async () => {
+    console.error('Not Implemented Error');
+  };
 
-    if (friend) {
-        weight += 2;
-    }
+  calculateWeight = (user1, user2) => {
+    let weight = 4;
     
     if (String(user1.favoriteFood) === String(user2.favoriteFood)) {
-        weight += 1;
+        weight -= 1;
     }
     if (String(user1.favoriteDrink) === String(user2.favoriteDrink)) {
-        weight += 1;
+        weight -= 1;
     }
     if (String(user1.livingCountry) === String(user2.livingCountry)) {
-        weight += 1;
+        weight -= 1;
     }
 
     return weight;
   };
 
-  addVertex = (adjList, v) => {
-    adjList.set(v, []);
+  addVertex = (v) => {
+    this.adjList.set(v, []);
   }
 
-  addEdge = (adjList, v, w, weight) =>{
-    if (!adjList.has(v)) this.addVertex(adjList, v);
-    // if (!this.adjList.has(w)) this.addVertex(w);
+  addEdge = (v, w, weight) =>{
+    if (!this.adjList.has(v)) this.addVertex(v);
+    if (!this.adjList.has(w)) this.addVertex(w);
 
-    if (!adjList.get(v).find((neighbor) => neighbor.node === w)) {
-        adjList.get(v).push({ node: w, weight })
+    if (!this.adjList.get(v).find((neighbor) => neighbor.node === w)) {
+        this.adjList.get(v).push({ node: w, weight })
     };
-    // if (!this.adjList.get(w).find((neighbor) => neighbor.node === v)) {
-    //     this.adjList.get(w).push({ node: v, weight })
-    // };
+    if (!this.adjList.get(w).find((neighbor) => neighbor.node === v)) {
+        this.adjList.get(w).push({ node: v, weight })
+    };
     console.log(`Added edge between ${v} and ${w} with weight ${weight}`);
   }
 
-  dijkstra = (adjList, start) => {
+  dijkstra = (start) => {
+    //bfs with weight
     const dist = new Map();
     const visited = new Map();
     var queue = [];
 
-    adjList.forEach((_, key) => {
+    this.adjList.forEach((_, key) => {
       dist.set(key, INF);
       visited.set(key, false);
     });
@@ -113,7 +115,7 @@ class Graph {
       if (visited.get(u)) continue;
       visited.set(u, true);
 
-      adjList.get(u).forEach((neighbor) => {
+      this.adjList.get(u).forEach((neighbor) => {
         const { node: v, weight } = neighbor;
         if (dist.get(v) > dist.get(u) + weight) {
           dist.set(v, dist.get(u) + weight);
@@ -124,6 +126,29 @@ class Graph {
 
     return dist;
   }
+
+  dfs = (start) => {
+    
+    const dfsMap = [];
+
+    function dfsRec(adjList, start, visited) {
+      visited.set(start, true);
+      console.log('dfs',start);
+      dfsMap.push(start);
+
+      adjList.get(start).forEach((neighbor) => {
+        if (!visited.get(neighbor.node)) {
+          dfsRec(adjList, neighbor.node, visited);
+        }
+      });
+    }
+
+    const visited = new Map();
+    this.adjList.forEach((_, key) => visited.set(key, false));
+    dfsRec(this.adjList, start, visited);
+
+    return dfsMap;
+  };
 
   dumpGraph = async () => {
     const filePath = path.join(process.env.GRAPH_JSON_PATH, 'graph.json');
